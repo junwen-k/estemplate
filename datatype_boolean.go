1987c94404c870242eac197ddad4d239e2d67172
Kwan Jun Wen <junwenkwan97@gmail.com>
2019-12-15 23:23:48 +0800

    Add boolean datatype

---
+++ datatype_boolean.go
@@ -0,0 +1,140 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import "fmt"
+
+// DatatypeBoolean Core Datatype for boolean which accept JSON true and false
+// values, but can also accept strings which are interpreted as either true or false:
+//
+// False values: false, "false"
+// True values: true, "true"
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/boolean.html
+// for details.
+type DatatypeBoolean struct {
+	Datatype
+	name string
+
+	// fields specific to boolean datatype
+	boost     *int
+	docValues *bool
+	index     *bool
+	nullValue *interface{}
+	store     *bool
+}
+
+// NewDatatypeBoolean initializes a new DatatypeBoolean.
+func NewDatatypeBoolean(name string) *DatatypeBoolean {
+	return &DatatypeBoolean{
+		name: name,
+	}
+}
+
+// Name is the key of the Boolean Property.
+func (b *DatatypeBoolean) Name() string {
+	return b.name
+}
+
+//
+// Boost sets Mapping field-level query time boosting. Defaults to 1.0.
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/mapping-boost.html
+// for details.
+func (b *DatatypeBoolean) Boost(boost int) *DatatypeBoolean {
+	b.boost = &boost
+	return b
+}
+
+// DocValues sets whether if the field should be stored on disk in a column-stride fashion
+// so that it can later be used for sorting, aggregations, or scripting.
+//
+// Defaults to true.
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/doc-values.html
+// for details.
+func (b *DatatypeBoolean) DocValues(docValues bool) *DatatypeBoolean {
+	b.docValues = &docValues
+	return b
+}
+
+//
+// Index sets whether if the field should be searchable. Defaults to true.
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/mapping-index.html
+// for details.
+func (b *DatatypeBoolean) Index(index bool) *DatatypeBoolean {
+	b.index = &index
+	return b
+}
+
+// NullValue sets any of the true and false values which is substituted for
+//
+// any explicit null values. Defaults to null.
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/null-value.html
+// for details.
+func (b *DatatypeBoolean) NullValue(nullValue interface{}) *DatatypeBoolean {
+	b.nullValue = &nullValue
+	return b
+}
+
+// Store sets whether the field value should be stored and retrievable separately
+//
+// from the `_source` field. Defaults to false.
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/mapping-store.html
+// for details.
+func (b *DatatypeBoolean) Store(store bool) *DatatypeBoolean {
+	b.store = &store
+	return b
+}
+
+// Validate validates DatatypeBoolean.
+func (b *DatatypeBoolean) Validate(includeName bool) error {
+	var invalid []string
+	if includeName && b.name == "" {
+		invalid = append(invalid, "Name")
+	}
+	if len(invalid) > 0 {
+		return fmt.Errorf("missing required fields: %v", invalid)
+	}
+	return nil
+}
+
+// Source returns the serializable JSON for the source builder.
+func (b *DatatypeBoolean) Source(includeName bool) (interface{}, error) {
+	// {
+	// 	"test": {
+	// 		"type": "boolean",
+	// 		"boost": 2,
+	// 		"doc_values": true,
+	// 		"index": true,
+	// 		"null_value": "true", // false
+	// 		"store": true,
+	// 	}
+	// }
+	options := make(map[string]interface{})
+	options["type"] = "boolean"
+
+	if b.boost != nil {
+		options["boost"] = b.boost
+	}
+	if b.docValues != nil {
+		options["doc_values"] = b.docValues
+	}
+	if b.index != nil {
+		options["index"] = b.index
+	}
+	if b.nullValue != nil {
+		options["null_value"] = b.nullValue
+	}
+	if b.store != nil {
+		options["store"] = b.store
+	}
+
+	if !includeName {
+		return options, nil
+	}
+
+	source := make(map[string]interface{})
+	source[b.name] = options
+	return source, nil
+}
---
+++ datatype_boolean_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestDatatypeBooleanSerialization(t *testing.T) {
+	tests := []struct {
+		desc        string
+		b           *DatatypeBoolean
+		includeName bool
+		expected    string
+	}{
+		// #0
+		{
+			desc:        "Include Name with Index.",
+			b:           NewDatatypeBoolean("test").Index(true),
+			includeName: true,
+			expected:    `{"test":{"index":true,"type":"boolean"}}`,
+		},
+		// #1
+		{
+			desc:        "Exclude Name with Index and Store.",
+			b:           NewDatatypeBoolean("test").Index(true).Store(true),
+			includeName: false,
+			expected:    `{"index":true,"store":true,"type":"boolean"}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			src, err := test.b.Source(test.includeName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := json.Marshal(src)
+			if err != nil {
+				t.Fatalf("marshaling to JSON failed: %v", err)
+			}
+			got := string(data)
+			if got != test.expected {
+				t.Errorf("expected\n%s\n,got:\n%s", test.expected, got)
+			}
+		})
+	}
+}
