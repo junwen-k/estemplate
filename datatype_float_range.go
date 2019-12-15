a6d383b0df00e2a7f97d5476da148ce58932fe60
Kwan Jun Wen <junwenkwan97@gmail.com>
2019-12-16 00:05:01 +0800

    Add float range datatype

---
+++ datatype_float_range.go
@@ -0,0 +1,121 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import "fmt"
+
+// DatatypeFloatRange Core Datatype for float range.
+// A range of single-precision 32-bit IEEE 754 floating point values.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/range.html
+// for details.
+type DatatypeFloatRange struct {
+	Datatype
+	name string
+
+	// fields specific to float range datatype
+	coerce *bool
+	boost  *int
+	index  *bool
+	store  *bool
+}
+
+// NewDatatypeFloatRange initializes a new DatatypeFloatRange.
+func NewDatatypeFloatRange(name string) *DatatypeFloatRange {
+	return &DatatypeFloatRange{
+		name: name,
+	}
+}
+
+// Name is the key of the Byte Property.
+func (r *DatatypeFloatRange) Name() string {
+	return r.name
+}
+
+// Coerce sets whether if the field should be coerced, attempting to clean up
+// dirty values to fit the datatype. Defaults to true.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/coerce.html
+// for details.
+func (r *DatatypeFloatRange) Coerce(coerce bool) *DatatypeFloatRange {
+	r.coerce = &coerce
+	return r
+}
+
+// Boost sets Mapping field-level query time boosting. Defaults to 1.0.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/mapping-boost.html
+// for details.
+func (r *DatatypeFloatRange) Boost(boost int) *DatatypeFloatRange {
+	r.boost = &boost
+	return r
+}
+
+// Index sets whether if the field should be searchable. Defaults to true.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/mapping-index.html
+// for details.
+func (r *DatatypeFloatRange) Index(index bool) *DatatypeFloatRange {
+	r.index = &index
+	return r
+}
+
+// Store sets whether the field value should be stored and retrievable separately
+// from the `_source` field. Defaults to false.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/mapping-store.html
+// for details.
+func (r *DatatypeFloatRange) Store(store bool) *DatatypeFloatRange {
+	r.store = &store
+	return r
+}
+
+// Validate validates DatatypeFloatRange.
+func (r *DatatypeFloatRange) Validate(includeName bool) error {
+	var invalid []string
+	if includeName && r.name == "" {
+		invalid = append(invalid, "Name")
+	}
+	if len(invalid) > 0 {
+		return fmt.Errorf("missing required fields: %v", invalid)
+	}
+	return nil
+}
+
+// Source returns the serializable JSON for the source builder.
+func (r *DatatypeFloatRange) Source(includeName bool) (interface{}, error) {
+	// {
+	// 	"test": {
+	// 		"type": "float_range",
+	// 		"coerce": true,
+	// 		"boost": 2,
+	// 		"index": true,
+	// 		"store": true
+	// 	}
+	// }
+	options := make(map[string]interface{})
+	options["type"] = "float_range"
+
+	if r.coerce != nil {
+		options["coerce"] = r.coerce
+	}
+	if r.boost != nil {
+		options["boost"] = r.boost
+	}
+	if r.index != nil {
+		options["index"] = r.index
+	}
+	if r.store != nil {
+		options["store"] = r.store
+	}
+
+	if !includeName {
+		return options, nil
+	}
+
+	source := make(map[string]interface{})
+	source[r.name] = options
+	return source, nil
+}
---
+++ datatype_float_range_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestDatatypeFloatRangeSerialization(t *testing.T) {
+	tests := []struct {
+		desc        string
+		r           *DatatypeFloatRange
+		includeName bool
+		expected    string
+	}{
+		// #0
+		{
+			desc:        "Include Name with Index.",
+			r:           NewDatatypeFloatRange("test").Index(true),
+			includeName: true,
+			expected:    `{"test":{"index":true,"type":"float_range"}}`,
+		},
+		// #1
+		{
+			desc:        "Exclude Name with Index and Coerce.",
+			r:           NewDatatypeFloatRange("test").Index(true).Coerce(true),
+			includeName: false,
+			expected:    `{"coerce":true,"index":true,"type":"float_range"}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			src, err := test.r.Source(test.includeName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := json.Marshal(src)
+			if err != nil {
+				t.Fatalf("marshaling to JSON failed: %v", err)
+			}
+			got := string(data)
+			if got != test.expected {
+				t.Errorf("expected\n%s\n,got:\n%s", test.expected, got)
+			}
+		})
+	}
+}
