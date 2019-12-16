b966093b3c675323dcc1c80ee68bec6f32bbc0e1
Kwan Jun Wen <junwenkwan97@gmail.com>
2019-12-16 21:52:16 +0800

    Add geo point datatype

---
+++ datatype_geo_point.go
@@ -0,0 +1,108 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import "fmt"
+
+// DatatypeGeoPoint Geo Datatype for latitude-longitude pairs, which can be used in:
+// - to find geo-points within a bounding box, within a certain distance of a central point, or within a polygon.
+// - to aggregate documents geographically or by distance from a central point.
+// - to integrate distance into a document’s relevance score.
+// - to sort documents by distance.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/geo-point.html
+// for details.
+type DatatypeGeoPoint struct {
+	Datatype
+	name string
+
+	// fields specific to geo point datatype
+	ignoreMalformed *bool
+	ignoreZValue    *bool
+	nullValue       interface{}
+}
+
+// NewDatatypeGeoPoint initializes a new DatatypeGeoPoint.
+func NewDatatypeGeoPoint(name string) *DatatypeGeoPoint {
+	return &DatatypeGeoPoint{
+		name: name,
+	}
+}
+
+// Name returns field key for the Datatype.
+func (p *DatatypeGeoPoint) Name() string {
+	return p.name
+}
+
+// IgnoreMalformed sets whether if the field should ignore malformed geo-points.
+// Defaults to false.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/ignore-malformed.html
+// for details.
+func (p *DatatypeGeoPoint) IgnoreMalformed(ignoreMalformed bool) *DatatypeGeoPoint {
+	p.ignoreMalformed = &ignoreMalformed
+	return p
+}
+
+// IgnoreZValue sets whether if the field should ignore the third dimension when three
+// dimension points is received. Defaults to true.
+func (p *DatatypeGeoPoint) IgnoreZValue(ignoreZValue bool) *DatatypeGeoPoint {
+	p.ignoreZValue = &ignoreZValue
+	return p
+}
+
+// NullValue sets a geopoint value which is substituted for any explicit null values.
+// Defaults to null.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/null-value.html
+// for details.
+func (p *DatatypeGeoPoint) NullValue(nullValue interface{}) *DatatypeGeoPoint {
+	p.nullValue = nullValue
+	return p
+}
+
+// Validate validates DatatypeGeoPoint.
+func (p *DatatypeGeoPoint) Validate(includeName bool) error {
+	var invalid []string
+	if includeName && p.name == "" {
+		invalid = append(invalid, "Name")
+	}
+	if len(invalid) > 0 {
+		return fmt.Errorf("missing required fields: %v", invalid)
+	}
+	return nil
+}
+
+// Source returns the serializable JSON for the source builder.
+func (p *DatatypeGeoPoint) Source(includeName bool) (interface{}, error) {
+	// {
+	// 	"test": {
+	// 		"type": "geo_point",
+	// 		"ignore_malformed": true,
+	// 		"ignore_z_value": true,
+	// 		"null_value": [ 0, 0 ]
+	// 	}
+	// }
+	options := make(map[string]interface{})
+	options["type"] = "geo_point"
+
+	if p.ignoreMalformed != nil {
+		options["ignore_malformed"] = p.ignoreMalformed
+	}
+	if p.ignoreZValue != nil {
+		options["ignore_z_value"] = p.ignoreZValue
+	}
+	if p.nullValue != nil {
+		options["null_value"] = p.nullValue
+	}
+
+	if !includeName {
+		return options, nil
+	}
+
+	source := make(map[string]interface{})
+	source[p.name] = options
+	return source, nil
+}
---
+++ datatype_geo_point_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestDatatypeGeoPointSerialization(t *testing.T) {
+	tests := []struct {
+		desc        string
+		p           *DatatypeGeoPoint
+		includeName bool
+		expected    string
+	}{
+		// #0
+		{
+			desc:        "Include Name with IgnoreZValue.",
+			p:           NewDatatypeGeoPoint("test").IgnoreZValue(true),
+			includeName: true,
+			expected:    `{"test":{"ignore_z_value":true,"type":"geo_point"}}`,
+		},
+		// #1
+		{
+			desc:        "Exclude Name with NullValue.",
+			p:           NewDatatypeGeoPoint("test").NullValue([]int{0, 0}),
+			includeName: false,
+			expected:    `{"null_value":[0,0],"type":"geo_point"}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			src, err := test.p.Source(test.includeName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := json.Marshal(src)
+			if err != nil {
+				t.Fatalf("marshaling to JSON failed: %v", err)
+			}
+			got := string(data)
+			if got != test.expected {
+				t.Errorf("expected\n%s\n,got:\n%s", test.expected, got)
+			}
+		})
+	}
+}
