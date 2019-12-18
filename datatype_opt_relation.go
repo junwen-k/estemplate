21d114bf153e44e4392ac56cc873e25b18b524a4
Kwan Jun Wen <junwenkwan97@gmail.com>
2019-12-19 00:21:41 +0800

    Add relation datatype option

---
+++ datatype_opt_relation.go
@@ -0,0 +1,65 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+// Relation Datatype parameter that defines a set of possible relations within
+// the documents, each relation being a parent name and a child name.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/parent-join.html#parent-join
+// for details.
+type Relation struct {
+	parent   string
+	children []string
+}
+
+// NewRelation initializes a new Relation.
+func NewRelation(parent string, childrens ...string) *Relation {
+	return &Relation{
+		parent:   parent,
+		children: childrens,
+	}
+}
+
+// Name returns field key (parent) for Datatype option.
+func (r *Relation) Name() string {
+	return r.parent
+}
+
+// Parent sets parent for the join relations.
+func (r *Relation) Parent(parent string) *Relation {
+	r.parent = parent
+	return r
+}
+
+// Children sets children(s) for the join relations.
+func (r *Relation) Children(childrens ...string) *Relation {
+	r.children = append(r.children, childrens...)
+	return r
+}
+
+// Source returns the serializable JSON for the source builder.
+func (r *Relation) Source(includeName bool) (interface{}, error) {
+	// "parent_1": ["children_1", "children_2"]
+
+	var value interface{}
+	switch {
+	case len(r.children) > 1:
+		value = r.children
+		break
+	case len(r.children) == 1:
+		value = r.children[0]
+		break
+	default:
+		value = ""
+	}
+
+	if !includeName {
+		return value, nil
+	}
+
+	source := make(map[string]interface{})
+	source[r.parent] = value
+	return source, nil
+}
---
+++ datatype_opt_relation_test.go
@@ -0,0 +1,57 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestRelationSerialization(t *testing.T) {
+	tests := []struct {
+		desc        string
+		r           *Relation
+		includeName bool
+		expected    string
+	}{
+		// #0
+		{
+			desc:        "Include Name without children.",
+			r:           NewRelation("parent"),
+			includeName: true,
+			expected:    `{"parent":""}`,
+		},
+		// #1
+		{
+			desc:        "Exclude Name with one children.",
+			r:           NewRelation("parent", "children_1"),
+			includeName: false,
+			expected:    `"children_1"`,
+		},
+		// #2
+		{
+			desc:        "Exclude Name with multiple childrens.",
+			r:           NewRelation("parent", "children_1", "children_2").Children("children_3"),
+			includeName: false,
+			expected:    `["children_1","children_2","children_3"]`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			src, err := test.r.Source(test.includeName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := json.Marshal(src)
+			if err != nil {
+				t.Fatalf("marshaling to JSON failed: %v", err)
+			}
+			got := string(data)
+			if got != test.expected {
+				t.Errorf("expected\n%s\n,got:\n%s", test.expected, got)
+			}
+		})
+	}
+}
