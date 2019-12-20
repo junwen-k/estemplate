5f451038d02d018228260c330feadd414966aee4
Kwan Jun Wen <junwenkwan97@gmail.com>
2019-12-20 17:03:27 +0800

    Add rank features datatype

---
+++ datatype_rank_features.go
@@ -0,0 +1,63 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import "fmt"
+
+// DatatypeRankFeatures Specialised Datatype that can index numeric feature vectors,
+// so that they can later be used to boost documents in queries with a `rank_feature`
+// query.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/rank-features.html
+// for details.
+type DatatypeRankFeatures struct {
+	Datatype
+	name string
+
+	// fields specific to rank features datatype
+}
+
+// NewDatatypeRankFeatures initializes a new DatatypeRankFeatures.
+func NewDatatypeRankFeatures(name string) *DatatypeRankFeatures {
+	return &DatatypeRankFeatures{
+		name: name,
+	}
+}
+
+// Name returns field key for the Datatype.
+func (f *DatatypeRankFeatures) Name() string {
+	return f.name
+}
+
+// Validate validates DatatypeRankFeatures.
+func (f *DatatypeRankFeatures) Validate(includeName bool) error {
+	var invalid []string
+	if includeName && f.name == "" {
+		invalid = append(invalid, "Name")
+	}
+	if len(invalid) > 0 {
+		return fmt.Errorf("missing required fields: %v", invalid)
+	}
+	return nil
+}
+
+// Source returns the serializable JSON for the source builder.
+func (f *DatatypeRankFeatures) Source(includeName bool) (interface{}, error) {
+	// {
+	// 	"name": {
+	// 		"type": "rank_features"
+	// 	}
+	// }
+	options := make(map[string]interface{})
+	options["type"] = "rank_features"
+
+	if !includeName {
+		return options, nil
+	}
+
+	source := make(map[string]interface{})
+	source[f.name] = options
+	return source, nil
+}
---
+++ datatype_rank_features_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestDatatypeRankFeaturesSerialization(t *testing.T) {
+	tests := []struct {
+		desc        string
+		f           *DatatypeRankFeatures
+		includeName bool
+		expected    string
+	}{
+		// #0
+		{
+			desc:        "Include Name.",
+			f:           NewDatatypeRankFeatures("test"),
+			includeName: true,
+			expected:    `{"test":{"type":"rank_features"}}`,
+		},
+		// #1
+		{
+			desc:        "Exclude Name.",
+			f:           NewDatatypeRankFeatures("test"),
+			includeName: false,
+			expected:    `{"type":"rank_features"}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			src, err := test.f.Source(test.includeName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := json.Marshal(src)
+			if err != nil {
+				t.Fatalf("marshaling to JSON failed: %v", err)
+			}
+			got := string(data)
+			if got != test.expected {
+				t.Errorf("expected\n%s\n,got:\n%s", test.expected, got)
+			}
+		})
+	}
+}
