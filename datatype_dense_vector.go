efaa8e2a79974e4ba5b827c483e418eb23fae763
Kwan Jun Wen <junwenkwan97@gmail.com>
2019-12-20 17:15:55 +0800

    Add dense vector datatype

---
+++ datatype_dense_vector.go
@@ -0,0 +1,80 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"fmt"
+)
+
+// DatatypeDenseVector Specialised Datatype that stores dense vectors of float
+// values. The maximum number of dimensions that can be in a vector should not exceed
+// 1024.
+// ! Experimental and may be changed or removed completely in a future release.
+// See
+// for details.
+type DatatypeDenseVector struct {
+	Datatype
+	name string
+
+	// fields specific to dense vector datatype
+	dims *int
+}
+
+// NewDatatypeDenseVector initializes a new DatatypeDenseVector.
+func NewDatatypeDenseVector(name string) *DatatypeDenseVector {
+	return &DatatypeDenseVector{
+		name: name,
+	}
+}
+
+// Name returns field key for the Datatype.
+func (v *DatatypeDenseVector) Name() string {
+	return v.name
+}
+
+// Dims sets the number of dimensions in the vector. Internally, each document's dense
+// vector is encoded as a binary doc value. Its size in bytes is equal to 4 * dims + 4,
+// where dims - the number of the vector's dimensions.
+func (v *DatatypeDenseVector) Dims(dims int) *DatatypeDenseVector {
+	v.dims = &dims
+	return v
+}
+
+// Validate validates DatatypeDenseVector.
+func (v *DatatypeDenseVector) Validate(includeName bool) error {
+	var invalid []string
+	if includeName && v.name == "" {
+		invalid = append(invalid, "Name")
+	}
+	// TODO: validate dims
+	if len(invalid) > 0 {
+		return fmt.Errorf("missing required fields: %v", invalid)
+	}
+	return nil
+}
+
+// Source returns the serializable JSON for the source builder.
+func (v *DatatypeDenseVector) Source(includeName bool) (interface{}, error) {
+	// {
+	// 	"test": {
+	// 		"type": "dense_vector",
+	// 		"dims": 3
+	// 	}
+	// }
+	options := make(map[string]interface{})
+	options["type"] = "dense_vector"
+
+	if v.dims != nil {
+		options["dims"] = v.dims
+	}
+
+	if !includeName {
+		return options, nil
+	}
+
+	source := make(map[string]interface{})
+	source[v.name] = options
+	return source, nil
+}
---
+++ datatype_dense_vector_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestDatatypeDenseVectorSerialization(t *testing.T) {
+	tests := []struct {
+		desc        string
+		v           *DatatypeDenseVector
+		includeName bool
+		expected    string
+	}{
+		// #0
+		{
+			desc:        "Include Name with Dims.",
+			v:           NewDatatypeDenseVector("test").Dims(3),
+			includeName: true,
+			expected:    `{"test":{"dims":3,"type":"dense_vector"}}`,
+		},
+		// #1
+		{
+			desc:        "Exclude Name.",
+			v:           NewDatatypeDenseVector("test"),
+			includeName: false,
+			expected:    `{"type":"dense_vector"}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			src, err := test.v.Source(test.includeName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := json.Marshal(src)
+			if err != nil {
+				t.Fatalf("marshaling to JSON failed: %v", err)
+			}
+			got := string(data)
+			if got != test.expected {
+				t.Errorf("expected\n%s\n,got:\n%s", test.expected, got)
+			}
+		})
+	}
+}
