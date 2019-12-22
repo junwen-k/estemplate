1e87022fc192a68a5891decf2ad5264de326a7c2
Kwan Jun Wen <junwenkwan97@gmail.com>
2019-12-22 13:26:48 +0800

    Add shape datatype

---
+++ datatype_shape.go
@@ -0,0 +1,116 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import "fmt"
+
+// DatatypeShape Specialised Datatype for arbitrary cartesian geometries. The
+// Datatype facilitates the indexing of and searching with arbitrary `x`, `y`
+// cartesian shapes such as rectangles and polygons.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/shape.html
+// for details.
+type DatatypeShape struct {
+	Datatype
+	name string
+
+	// fields specific to shape datatype
+	orientation     string
+	ignoreMalformed *bool
+	ignoreZValue    *bool
+	coerce          *bool
+}
+
+// NewDatatypeShape initializes a new DatatypeShape.
+func NewDatatypeShape(name string) *DatatypeShape {
+	return &DatatypeShape{
+		name: name,
+	}
+}
+
+// Name returns field key for the Datatype.
+func (s *DatatypeShape) Name() string {
+	return s.name
+}
+
+// Orientation sets the orientation on how to interpret vertex order for polygons / multipolygons.
+// Defines one of two coordinate system rule, Right-hand or Left-hand.
+// Can be set to the following values:
+// "right", "ccw", "counterclockwise" - Right-hand rule.
+// "left", "cw", "clockwise" - Left-hand rule.
+// Defaults to "ccw".
+func (s *DatatypeShape) Orientation(orientation string) *DatatypeShape {
+	s.orientation = orientation
+	return s
+}
+
+// IgnoreMalformed sets whether if the field should ignore malformed GeoJSON or WKT shapes.
+// Defaults to false.
+func (s *DatatypeShape) IgnoreMalformed(ignoreMalformed bool) *DatatypeShape {
+	s.ignoreMalformed = &ignoreMalformed
+	return s
+}
+
+// IgnoreZValue sets whether if the field should ignore the third dimension when three
+// dimension points is received. Defaults to true.
+func (s *DatatypeShape) IgnoreZValue(ignoreZValue bool) *DatatypeShape {
+	s.ignoreZValue = &ignoreZValue
+	return s
+}
+
+// Coerce sets whether if unclosed linear rings in polygons will be automatically closed.
+// Defaults to false.
+func (s *DatatypeShape) Coerce(coerce bool) *DatatypeShape {
+	s.coerce = &coerce
+	return s
+}
+
+// Validate validates DatatypeShape.
+func (s *DatatypeShape) Validate(includeName bool) error {
+	var invalid []string
+	if includeName && s.name == "" {
+		invalid = append(invalid, "Name")
+	}
+	if len(invalid) > 0 {
+		return fmt.Errorf("missing required fields: %v", invalid)
+	}
+	return nil
+}
+
+// Source returns the serializable JSON for the source builder.
+func (s *DatatypeShape) Source(includeName bool) (interface{}, error) {
+	// {
+	// 	"test": {
+	// 		"type": "shape",
+	// 		"orientation": "ccw",
+	// 		"ignore_malformed": true,
+	// 		"ignore_z_value": true,
+	// 		"coerce": true
+	// 	}
+	// }
+	options := make(map[string]interface{})
+	options["type"] = "shape"
+
+	if s.orientation != "" {
+		options["orientation"] = s.orientation
+	}
+	if s.ignoreMalformed != nil {
+		options["ignore_malformed"] = s.ignoreMalformed
+	}
+	if s.ignoreZValue != nil {
+		options["ignore_z_value"] = s.ignoreZValue
+	}
+	if s.coerce != nil {
+		options["coerce"] = s.coerce
+	}
+
+	if !includeName {
+		return options, nil
+	}
+
+	source := make(map[string]interface{})
+	source[s.name] = options
+	return source, nil
+}
---
+++ datatype_shape_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestDatatypeShapeSerialization(t *testing.T) {
+	tests := []struct {
+		desc        string
+		s           *DatatypeShape
+		includeName bool
+		expected    string
+	}{
+		// #0
+		{
+			desc:        "Include Name with IgnoreZValue.",
+			s:           NewDatatypeShape("test").IgnoreZValue(true),
+			includeName: true,
+			expected:    `{"test":{"ignore_z_value":true,"type":"shape"}}`,
+		},
+		// #1
+		{
+			desc:        "Exclude Name with Coerce.",
+			s:           NewDatatypeShape("test").Coerce(true),
+			includeName: false,
+			expected:    `{"coerce":true,"type":"shape"}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			src, err := test.s.Source(test.includeName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := json.Marshal(src)
+			if err != nil {
+				t.Fatalf("marshaling to JSON failed: %v", err)
+			}
+			got := string(data)
+			if got != test.expected {
+				t.Errorf("expected\n%s\n,got:\n%s", test.expected, got)
+			}
+		})
+	}
+}
