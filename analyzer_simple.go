2cd5c9357fc237d1449943ad7ce15e51165aff02
Kwan Jun Wen <junwenkwan97@gmail.com>
2019-12-23 23:46:06 +0800

    Add simple analyzer

---
+++ analyzer_simple.go
@@ -0,0 +1,62 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import "fmt"
+
+// AnalyzerSimple divdes text into terms whenever it encounters any whitespace
+// character. It does not lowercase terms.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/analysis-simple-analyzer.html
+// for details.
+type AnalyzerSimple struct {
+	Analyzer
+	name string
+
+	// fields specific to simple analyzer
+}
+
+// NewAnalyzerSimple initializes a new AnalyzerSimple.
+func NewAnalyzerSimple(name string) *AnalyzerSimple {
+	return &AnalyzerSimple{
+		name: name,
+	}
+}
+
+// Name returns field key for the Analyzer.
+func (s *AnalyzerSimple) Name() string {
+	return s.name
+}
+
+// Validate validates AnalyzerSimple.
+func (s *AnalyzerSimple) Validate(includeName bool) error {
+	var invalid []string
+	if includeName && s.name == "" {
+		invalid = append(invalid, "Name")
+	}
+	if len(invalid) > 0 {
+		return fmt.Errorf("missing required fields: %v", invalid)
+	}
+	return nil
+}
+
+// Source returns the serializable JSON for the source builder.
+func (s *AnalyzerSimple) Source(includeName bool) (interface{}, error) {
+	// {
+	// 	"test": {
+	// 		"type": "simple"
+	// 	}
+	// }
+	options := make(map[string]interface{})
+	options["type"] = "simple"
+
+	if !includeName {
+		return options, nil
+	}
+
+	source := make(map[string]interface{})
+	source[s.name] = options
+	return source, nil
+}
---
+++ analyzer_simple_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestAnalyzerSimpleSerialization(t *testing.T) {
+	tests := []struct {
+		desc        string
+		m3          *AnalyzerSimple
+		includeName bool
+		expected    string
+	}{
+		// #0
+		{
+			desc:        "Include Name.",
+			m3:          NewAnalyzerSimple("test"),
+			includeName: true,
+			expected:    `{"test":{"type":"simple"}}`,
+		},
+		// #1
+		{
+			desc:        "Exclude Name.",
+			m3:          NewAnalyzerSimple("test"),
+			includeName: false,
+			expected:    `{"type":"simple"}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			src, err := test.m3.Source(test.includeName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := json.Marshal(src)
+			if err != nil {
+				t.Fatalf("marshaling to JSON failed: %v", err)
+			}
+			got := string(data)
+			if got != test.expected {
+				t.Errorf("expected\n%s\n,got:\n%s", test.expected, got)
+			}
+		})
+	}
+}
