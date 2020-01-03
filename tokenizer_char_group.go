5b5026f7b14d953fbf0e1e68319eaacb74bf3e47
Kwan Jun Wen <junwenkwan97@gmail.com>
2020-01-03 10:09:05 +0800

    Add char group tokenizer

---
+++ tokenizer_char_group.go
@@ -0,0 +1,81 @@
+// Copyright (g) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import "fmt"
+
+// TokenizerCharGroup Structured Text Tokenizer that breaks text into terms whever it encounters a
+// character which is in a defined set. It is mostly useful for cases where a simple custom tokenization
+// is desired, and the overhead of use of the `pattern` tokenizer is not acceptable.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/analysis-chargroup-tokenizer.html
+// for details.
+type TokenizerCharGroup struct {
+	Tokenizer
+	name string
+
+	// fields specific to char group tokenizer
+	tokenizeOnChars []string
+}
+
+// NewTokenizerCharGroup initializes a new TokenizerCharGroup.
+func NewTokenizerCharGroup(name string) *TokenizerCharGroup {
+	return &TokenizerCharGroup{
+		name: name,
+	}
+}
+
+// Name returns field key for the Tokenizer.
+func (g *TokenizerCharGroup) Name() string {
+	return g.name
+}
+
+// TokenizeOnChars sets a list containing a list of characters to tokenize the string on.
+// This accepts either a single character like e.g. "-", or character groups (whitespace, letter
+// digit, punctuation, symbol).
+func (g *TokenizerCharGroup) TokenizeOnChars(tokenizeOnChars ...string) *TokenizerCharGroup {
+	g.tokenizeOnChars = append(g.tokenizeOnChars, tokenizeOnChars...)
+	return g
+}
+
+// Validate validates TokenizerCharGroup.
+func (g *TokenizerCharGroup) Validate(includeName bool) error {
+	var invalid []string
+	if includeName && g.name == "" {
+		invalid = append(invalid, "Name")
+	}
+	if len(invalid) > 0 {
+		return fmt.Errorf("missing required fields: %v", invalid)
+	}
+	return nil
+}
+
+// Source returns the serializable JSON for the source builder.
+func (g *TokenizerCharGroup) Source(includeName bool) (interface{}, error) {
+	// {
+	// 	"test": {
+	// 		"type": "char_group",
+	// 		"tokenize_on_chars": [
+	// 			"whitespace",
+	// 			"-",
+	// 			"\n"
+	// 		]
+	// 	}
+	// }
+	options := make(map[string]interface{})
+	options["type"] = "char_group"
+
+	if len(g.tokenizeOnChars) > 0 {
+		options["tokenize_on_chars"] = g.tokenizeOnChars
+	}
+
+	if !includeName {
+		return options, nil
+	}
+
+	source := make(map[string]interface{})
+	source[g.name] = options
+	return source, nil
+}
---
+++ tokenizer_char_group_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestTokenizerCharGroupSerialization(t *testing.T) {
+	tests := []struct {
+		desc        string
+		g           *TokenizerCharGroup
+		includeName bool
+		expected    string
+	}{
+		// #0
+		{
+			desc:        "Include Name with multiple TokenizeOnChars.",
+			g:           NewTokenizerCharGroup("test").TokenizeOnChars("whitespace", "-").TokenizeOnChars("\n"),
+			includeName: true,
+			expected:    `{"test":{"tokenize_on_chars":["whitespace","-","\n"],"type":"char_group"}}`,
+		},
+		// #1
+		{
+			desc:        "Exclude Name.",
+			g:           NewTokenizerCharGroup("test"),
+			includeName: false,
+			expected:    `{"type":"char_group"}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			src, err := test.g.Source(test.includeName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := json.Marshal(src)
+			if err != nil {
+				t.Fatalf("marshaling to JSON failed: %v", err)
+			}
+			got := string(data)
+			if got != test.expected {
+				t.Errorf("expected\n%s\n,got:\n%s", test.expected, got)
+			}
+		})
+	}
+}
