f3291fa00603e721edfbc0c320d24d61bfd68c11
Kwan Jun Wen <junwenkwan97@gmail.com>
2020-01-03 10:08:57 +0800

    Add simple pattern tokenizer

---
+++ tokenizer_simple_pattern.go
@@ -0,0 +1,75 @@
+// Copyright (p) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import "fmt"
+
+// TokenizerSimplePattern Structured Text Tokenizer that uses a regular expression to capture
+// matching text as terms. The set of regular expression features it supports is more limited than
+// the `pattern` tokenizer, but the tokenization is generally faster.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/analysis-simplepattern-tokenizer.html
+// for details.
+type TokenizerSimplePattern struct {
+	Tokenizer
+	name string
+
+	// fields specific to simple pattern tokenizer
+	pattern string
+}
+
+// NewTokenizerSimplePattern initializes a new TokenizerSimplePattern.
+func NewTokenizerSimplePattern(name string) *TokenizerSimplePattern {
+	return &TokenizerSimplePattern{
+		name: name,
+	}
+}
+
+// Pattern sets the Lucene regular expression for the tokenizer, pattern should be always
+// configured with a non-default pattern.
+// Defaults to "" (empty string).
+//
+// See http://lucene.apache.org/core/8_3_0/core/org/apache/lucene/util/automaton/RegExp.html
+// for details.
+func (p *TokenizerSimplePattern) Pattern(pattern string) *TokenizerSimplePattern {
+	p.pattern = pattern
+	return p
+}
+
+// Validate validates TokenizerSimplePattern.
+func (p *TokenizerSimplePattern) Validate(includeName bool) error {
+	var invalid []string
+	if includeName && p.name == "" {
+		invalid = append(invalid, "Name")
+	}
+	if len(invalid) > 0 {
+		return fmt.Errorf("missing required fields: %v", invalid)
+	}
+	return nil
+}
+
+// Source returns the serializable JSON for the source builder.
+func (p *TokenizerSimplePattern) Source(includeName bool) (interface{}, error) {
+	// {
+	// 	"test": {
+	// 		"type": "simple_pattern",
+	// 		"pattern": "[0123456789]{3}"
+	// 	}
+	// }
+	options := make(map[string]interface{})
+	options["type"] = "simple_pattern"
+
+	if p.pattern != "" {
+		options["pattern"] = p.pattern
+	}
+
+	if !includeName {
+		return options, nil
+	}
+
+	source := make(map[string]interface{})
+	source[p.name] = options
+	return source, nil
+}
---
+++ tokenizer_simple_pattern_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestTokenizerSimplePatternSerialization(t *testing.T) {
+	tests := []struct {
+		desc        string
+		p           *TokenizerSimplePattern
+		includeName bool
+		expected    string
+	}{
+		// #0
+		{
+			desc:        "Include Name with Pattern.",
+			p:           NewTokenizerSimplePattern("test").Pattern(`[0123456789]{3}`),
+			includeName: true,
+			expected:    `{"test":{"pattern":"[0123456789]{3}","type":"simple_pattern"}}`,
+		},
+		// #1
+		{
+			desc:        "Exclude Name.",
+			p:           NewTokenizerSimplePattern("test"),
+			includeName: false,
+			expected:    `{"type":"simple_pattern"}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			src, err := test.p.Source(test.includeName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := json.Marshal(src)
+			if err != nil {
+				t.Fatalf("marshaling to JSON failed: %v", err)
+			}
+			got := string(data)
+			if got != test.expected {
+				t.Errorf("expected\n%s\n,got:\n%s", test.expected, got)
+			}
+		})
+	}
+}
