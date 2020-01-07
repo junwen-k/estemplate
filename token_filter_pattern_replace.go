a04175cfdc8a0caeeecd72d01e9a3adee49bc1a0
Kwan Jun Wen <junwenkwan97@gmail.com>
2020-01-07 23:43:36 +0800

    Add pattern replace token filter

---
+++ token_filter_pattern_replace.go
@@ -0,0 +1,86 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import "fmt"
+
+// TokenFilterPatternReplace token filter that allows to easily handle string replacements
+// based on a regular expression. The regular expression is defined using the `pattern` parameter,
+// and the replacement string can be provided using the `replacement` parameter.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/reference/7.5/analysis-pattern_replace-tokenfilter.html
+// for details.
+type TokenFilterPatternReplace struct {
+	TokenFilter
+	name string
+
+	// fields specific to pattern capture token filter
+	pattern     string
+	replacement string
+}
+
+// NewTokenFilterPatternReplace initializes a new TokenFilterPatternReplace.
+func NewTokenFilterPatternReplace(name string) *TokenFilterPatternReplace {
+	return &TokenFilterPatternReplace{
+		name: name,
+	}
+}
+
+// Name returns field key for the Token Filter.
+func (r *TokenFilterPatternReplace) Name() string {
+	return r.name
+}
+
+// Pattern sets the regular expression for matching tokens.
+func (r *TokenFilterPatternReplace) Pattern(pattern string) *TokenFilterPatternReplace {
+	r.pattern = pattern
+	return r
+}
+
+// Replacement sets the replacement string for matched tokens.
+func (r *TokenFilterPatternReplace) Replacement(replacement string) *TokenFilterPatternReplace {
+	r.replacement = replacement
+	return r
+}
+
+// Validate validates TokenFilterPatternReplace.
+func (r *TokenFilterPatternReplace) Validate(includeName bool) error {
+	var invalid []string
+	if includeName && r.name == "" {
+		invalid = append(invalid, "Name")
+	}
+	if len(invalid) > 0 {
+		return fmt.Errorf("missing required fields or invalid values: %v", invalid)
+	}
+	return nil
+}
+
+// Source returns the serializable JSON for the source builder.
+func (r *TokenFilterPatternReplace) Source(includeName bool) (interface{}, error) {
+	// {
+	// 	"test": {
+	// 		"type": "pattern_replace",
+	// 		"pattern": "\\",
+	// 		"replacement": "+",
+	// 	}
+	// }
+	options := make(map[string]interface{})
+	options["type"] = "pattern_replace"
+
+	if r.pattern != "" {
+		options["pattern"] = r.pattern
+	}
+	if r.replacement != "" {
+		options["replacement"] = r.replacement
+	}
+
+	if !includeName {
+		return options, nil
+	}
+
+	source := make(map[string]interface{})
+	source[r.name] = options
+	return source, nil
+}
---
+++ token_filter_pattern_replace_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestTokenFilterPatternReplaceSerialization(t *testing.T) {
+	tests := []struct {
+		desc        string
+		c           *TokenFilterPatternReplace
+		includeName bool
+		expected    string
+	}{
+		// #0
+		{
+			desc:        "Include Name with Patterns.",
+			c:           NewTokenFilterPatternReplace("test").Pattern(`\`),
+			includeName: true,
+			expected:    `{"test":{"pattern":"\\","type":"pattern_replace"}}`,
+		},
+		// #1
+		{
+			desc:        "Exclude Name with PreserveOriginal.",
+			c:           NewTokenFilterPatternReplace("test").Replacement("+"),
+			includeName: false,
+			expected:    `{"replacement":"+","type":"pattern_replace"}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			src, err := test.c.Source(test.includeName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := json.Marshal(src)
+			if err != nil {
+				t.Fatalf("marshaling to JSON failed: %v", err)
+			}
+			got := string(data)
+			if got != test.expected {
+				t.Errorf("expected\n%s\n,got:\n%s", test.expected, got)
+			}
+		})
+	}
+}
