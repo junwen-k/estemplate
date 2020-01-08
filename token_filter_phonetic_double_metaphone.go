b07fecf5dc1c565ee62d0ab536ebfa2d8630d2b2
Kwan Jun Wen <junwenkwan97@gmail.com>
2020-01-08 21:58:52 +0800

    Add phonetic double metaphone token filter

---
+++ token_filter_phonetic_double_metaphone.go
@@ -0,0 +1,91 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import "fmt"
+
+// TokenFilterPhoneticDoubleMetaphone (Plugin) token filter that provides token filters which
+// convert tokens to their phonetic representation using Soundex, Metaphone, and
+// a variety of other algorithms.
+//
+// See https://www.elastic.co/guide/en/elasticsearch/plugins/7.5/analysis-phonetic-token-filter.html
+// for details.
+type TokenFilterPhoneticDoubleMetaphone struct {
+	TokenFilter
+	name string
+
+	// fields specific to phonetic token beider morse filter
+	replace    *bool
+	maxCodeLen *int
+}
+
+// NewTokenFilterPhoneticDoubleMetaphone initializes a new TokenFilterPhoneticDoubleMetaphone.
+func NewTokenFilterPhoneticDoubleMetaphone(name string) *TokenFilterPhoneticDoubleMetaphone {
+	return &TokenFilterPhoneticDoubleMetaphone{
+		name: name,
+	}
+}
+
+// Name returns field key for the Token Filter.
+func (p *TokenFilterPhoneticDoubleMetaphone) Name() string {
+	return p.name
+}
+
+// Replace sets whether or not the original token should be replaced by
+// the phonetic token.
+// Defaults to true.
+func (p *TokenFilterPhoneticDoubleMetaphone) Replace(replace bool) *TokenFilterPhoneticDoubleMetaphone {
+	p.replace = &replace
+	return p
+}
+
+// MaxCodeLen sets the maximum length of the emitted metaphone token.
+// Defaults to 4.
+func (p *TokenFilterPhoneticDoubleMetaphone) MaxCodeLen(maxCodeLen int) *TokenFilterPhoneticDoubleMetaphone {
+	p.maxCodeLen = &maxCodeLen
+	return p
+}
+
+// Validate validates TokenFilterPhoneticDoubleMetaphone.
+func (p *TokenFilterPhoneticDoubleMetaphone) Validate(includeName bool) error {
+	var invalid []string
+	if includeName && p.name == "" {
+		invalid = append(invalid, "Name")
+	}
+	if len(invalid) > 0 {
+		return fmt.Errorf("missing required fields or invalid values: %v", invalid)
+	}
+	return nil
+}
+
+// Source returns the serializable JSON for the source builder.
+func (p *TokenFilterPhoneticDoubleMetaphone) Source(includeName bool) (interface{}, error) {
+	// {
+	// 	"test": {
+	// 		"type": "phonetic",
+	// 		"encoder": "double_metaphone",
+	// 		"replace": true,
+	// 		"max_code_len": 4
+	// 	}
+	// }
+	options := make(map[string]interface{})
+	options["type"] = "phonetic"
+	options["encoder"] = "double_metaphone"
+
+	if p.replace != nil {
+		options["replace"] = p.replace
+	}
+	if p.maxCodeLen != nil {
+		options["max_code_len"] = p.maxCodeLen
+	}
+
+	if !includeName {
+		return options, nil
+	}
+
+	source := make(map[string]interface{})
+	source[p.name] = options
+	return source, nil
+}
---
+++ token_filter_phonetic_double_metaphone_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package estemplate
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestTokenFilterPhoneticDoubleMetaphoneSerialization(t *testing.T) {
+	tests := []struct {
+		desc        string
+		p           *TokenFilterPhoneticDoubleMetaphone
+		includeName bool
+		expected    string
+	}{
+		// #0
+		{
+			desc:        "Include Name with Replace.",
+			p:           NewTokenFilterPhoneticDoubleMetaphone("test").Replace(true),
+			includeName: true,
+			expected:    `{"test":{"encoder":"double_metaphone","replace":true,"type":"phonetic"}}`,
+		},
+		// #1
+		{
+			desc:        "Exclude Name with MaxCodeLen.",
+			p:           NewTokenFilterPhoneticDoubleMetaphone("test").MaxCodeLen(4),
+			includeName: false,
+			expected:    `{"encoder":"double_metaphone","max_code_len":4,"type":"phonetic"}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			src, err := test.p.Source(test.includeName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			data, err := json.Marshal(src)
+			if err != nil {
+				t.Fatalf("marshaling to JSON failed: %v", err)
+			}
+			got := string(data)
+			if got != test.expected {
+				t.Errorf("expected\n%s\n,got:\n%s", test.expected, got)
+			}
+		})
+	}
+}
